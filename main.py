52406ca0ae891f4eead6ccbd5dc0ff429ab6c186
alperen-zeybek <alperenzeybek20005@gmail.com>
2026-08-30 22:14:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ KAYNAK DOKÜMAN BİLGİSİ:
     try:
         chat_completion = client.chat.completions.create(
             messages=messages,
-            model="llama-3.1-8b-instant",
+            model="llama3-8b-8192",
             temperature=0.6,
             max_tokens=600,
         )
